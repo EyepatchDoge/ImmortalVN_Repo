2e586ff3f7e6b8a519b533e394b8cecb87fab102
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-09-25 11:41:39 -0400

    Kas_Update5
    Added scene releated things such as a restart and exit game script, added more draggable rocks and built the game (outside of the repo)

---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Prototype/Scripts/RestartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    public void StartFromBeginning(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("You have left the game.");
+        }
+    }
+}
