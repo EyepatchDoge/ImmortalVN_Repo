7fe31541ddd2c6dcc968c8a664708323558afb79
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-09-25 21:27:28 -0400

    Update_9
    created placeholder main menu for game

--- ImmortalVisualNovel_Collab_Proj/Assets/Prototype/Scripts/RestartGame.cs
+++ ImmortalVisualNovel_Collab_Proj/Assets/Prototype/Scripts/RestartGame.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour
 {
-    public void StartFromBeginning(string scene)
+    public void NewScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
@@ -17,5 +17,11 @@ public class RestartGame : MonoBehaviour
             Application.Quit();
             Debug.Log("You have left the game.");
         }
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit has been sucessful");
     }
 }
