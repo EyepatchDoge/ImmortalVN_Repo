750d2817da1bfc1c0a741cac3bfbbc79af4ef182
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-10-05 13:07:34 -0400

    Kas_Update16
    - added "pipe" scripts for rotation puzzle

---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Script/RotationPuzzle/PipePuzzle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzle : MonoBehaviour
+{
+    public GameObject[] pipe;
+    public bool levelCompleted;
+    //public WaterManager waterMan;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelCompleted = false;
+    }
+
+    public void WinningConditions()
+    {
+        for (int i = 0; i < pipe.Length; i++)
+        {
+            var whatever = pipe[i].transform.localRotation.eulerAngles.z;
+            Mathf.RoundToInt(whatever);
+            if (Mathf.Abs( whatever )> -1 && Mathf.Abs(whatever) < 5)
+            {
+                levelCompleted = true;
+                
+
+            }
+
+            else
+            {
+                levelCompleted = false;
+                    break;
+            }
+        }
+
+        if (levelCompleted)
+        {
+            Debug.Log("Yass");
+           // waterMan.flushing.Play();
+        }
+
+        else{
+            Debug.Log("Dam, keep going buddy");
+        }
+    }
+}
---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Script/RotationPuzzle/PipeSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO", menuName = "ScriptableObjects/PipeSO", order = 1)]
+public class PipeSO : ScriptableObject
+{
+    public GameObject pipes;
+    
+}
