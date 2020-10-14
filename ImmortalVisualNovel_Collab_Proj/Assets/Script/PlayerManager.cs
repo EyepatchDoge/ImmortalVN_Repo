fd273c4bc5dc72932c95b613111c219cbd484a24
lxssa <lyssa.tino@gmail.com>
2020-10-14 19:29:47 -0400

    PlayerManager setup

---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Script/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    //personality points
+    public int kind = 0;
+    public int playful = 0;
+    public int aggressive = 0;
+
+    //increment personality points
+    void IncrementKind()
+    {
+        kind++;
+        Debug.Log("Kind points: " + kind);
+    }
+    void IncrementPlayful()
+    {
+        playful++;
+        Debug.Log("Playful points: " + playful);
+    }
+    void IncrementAggressive()
+    {
+        aggressive++;
+        Debug.Log("Aggressive points: " + aggressive);
+    }
+}
