a2d47d44745408b8e52796a69ff73f4fdac9bd90
lxssa <lyssa.tino@gmail.com>
2020-10-16 13:36:00 -0400

    Updated player manager

---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    //personality points
+    public int kind = 0;
+    public int playful = 0;
+    public int aggressive = 0;
+
+    //careers
+    public bool isFarmer = false;
+    public bool isCrafter = false;
+    public bool isButcher = false;
+
+    //increment personality points
+    void IncrementKind()
+    {
+        kind++;
+        Debug.Log("Kind points: " + kind);
+    }
+    void IncrementPlayful()
+    {
+        playful++;
+        Debug.Log("Playful points: " + playful);
+    }
+    void IncrementAggressive()
+    {
+        aggressive++;
+        Debug.Log("Aggressive points: " + aggressive);
+    }
+}
---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Scripts/RotationPuzzle/PipeRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeRotation : MonoBehaviour
+{
+    public PipePuzzle pP;
+
+    private void Awake()
+    {
+        pP = GetComponent<PipePuzzle>();
+    }
+
+    public void RotatePipe(GameObject pipe)
+    {
+
+        pipe.transform.localRotation = Quaternion.Euler( new Vector3(0,0, pipe.transform.localRotation.eulerAngles.z + 90));
+        //pipe.transform.Rotate(new Vector3(0, 0, pipe.transform.localRotation.eulerAngles.z + 90));
+        pP.WinningConditions();
+    }
+}
