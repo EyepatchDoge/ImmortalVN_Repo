93de38da4dd7dba95e7ccb29e96a60c130f29928
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-11-25 04:29:45 -0500

    kas_update74
    created an animated credits scene

---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Scripts/Kas' Scripts/CreditAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject returnBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   public void CreditsOn()
+    {
+        animator.SetBool("isCreditsOn", true);
+        Debug.Log("credits are rolling");
+    }
+
+    public void CreditsOff()
+    {
+        animator.SetBool("isCreditsOn", false);
+        Debug.Log("credits have finished");
+        returnBtn.SetActive(true);
+    }
+}
