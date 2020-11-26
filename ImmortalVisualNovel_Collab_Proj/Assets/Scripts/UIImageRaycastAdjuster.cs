96d7de7a8825b3d8ead59d37ae15d3eca13ae061
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-11-25 19:31:27 -0500

    kas_update76
    -fixed rotation puzzle

---
+++ ImmortalVisualNovel_Collab_Proj/Assets/Scripts/UIImageRaycastAdjuster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIImageRaycastAdjuster : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //make sure the image's read and write is enabled
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+
+}
