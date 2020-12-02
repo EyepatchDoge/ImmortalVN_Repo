29c2af681d8969c503ceb39b3ee02db45a45c291
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-12-01 21:04:40 -0500

    kas_update82
    made minor changes

--- ImmortalVisualNovel_Collab_Proj/Assets/Scripts/Kas' Scripts/RestartGame.cs
+++ ImmortalVisualNovel_Collab_Proj/Assets/Scripts/Kas' Scripts/RestartGame.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RestartGame : MonoBehaviour
 {
+    public GameObject mcPanel;
+    public GameObject narrativePanel;
+    public GameObject otherPanel;
+    public GameObject lowerNarrativePanel;
+    public GameObject momPanel;
+
+
     public void NewScene(string scene)
     {
         SceneManager.LoadScene(scene);
@@ -27,14 +35,25 @@ public class RestartGame : MonoBehaviour
 
     public void PauseGame()
     {
-        Time.timeScale = 0;
+       /* momPanel.SetActive(false);
+        mcPanel.SetActive(false);
+        otherPanel.SetActive(false);
+        narrativePanel.SetActive(false);
+        lowerNarrativePanel.SetActive(false);*/
+        
+       Time.timeScale = 0;
         Debug.Log("Game is paused");
     }
 
     public void ResumeGame()
     {
+        /*momPanel.SetActive(true);
+        mcPanel.SetActive(true);
+        otherPanel.SetActive(true);
+        narrativePanel.SetActive(true);
+        lowerNarrativePanel.SetActive(true);*/
         Time.timeScale = 1;
-        Debug.Log("Game has resumed");
+         Debug.Log("Game has resumed");
     }
 
    
